9a3d5e1e2ccca8a2a98e9e114229bfbcd7333c97
Salman Rameli <salmanrameli@hotmail.com>
2016-12-14 18:37:13 +0700

    client dapat menerima input dari keyboard dan mengirim input ke server

--- client.py
+++ client.py
@@ -1,27 +1,12 @@
 import socket
 
-a = []
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('127.0.0.1', 51000))
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 21))
+try:
+    while True:
+        message = raw_input()
+        client_socket.send(message)
 
-commands = ['USER salman\r\n', 'PASS 123\r\n', 'QUIT\r\n']
-
-i = 1
-
-while True:
-    try:
-        if i > len(commands):
-            msg = str(s.recv(1024))
-            break
-        s.send(commands[i - 1])
-        msg = str(s.recv(1024))
-        if "220" in msg:
-            msa = msg.replace('msg ', '').replace('220 ', '')
-            print msa
-        i += 1
-
-    except socket.error, exc:
-        # print exc
-        s.close()
-        break
+except KeyboardInterrupt:
+    client_socket.close()
